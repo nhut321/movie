0d77c86147e1b812786a72256655cef6c1e43087
nhut321 <znhut12z@gmail.com>
2022-05-18 02:45:38 +0700

    update DetailHero

--- src/components/DetailHero/index.js
+++ src/components/DetailHero/index.js
@@ -1,7 +1,9 @@
+// import { CircularProgressbar } from 'react-circular-progressbar';
 import { Row, Col } from 'react-bootstrap'
 import detaiMovieImg from '../../assets/detail-movie.jpg'
 import movieItem from '../../assets/movie-item.jpg'
 import './DetailHero.css'
+// import 'react-circular-progressbar/dist/styles.css'
 
 export default function DetailHero() {
 	return (
@@ -10,11 +12,66 @@ export default function DetailHero() {
 				<div className="detail-hero__mask"></div>
 				<Row className='h-100' style={{position: 'relative', zIndex: 2}}>
 					<Col xs='3' className='d-flex align-items-center'>
-						<div className="detail-hero__img ms-4">
+						<div className="detail-hero__img">
 							<img src={detaiMovieImg} alt="" />
 						</div>
 					</Col>
-					<Col xs='9'>123213213</Col>
+					<Col xs='9'>
+						<div className="detail-right">
+							<div className="detail-hero__title">
+								<h2>Logan</h2>
+							</div>
+							<div className="detail-genres">
+								<span className='me-2'>Action</span>
+								<span>Drama</span>
+							</div>
+							<div className="detail-actions">
+								<div className="detail-actions__rating">
+									<div className="detail-actions__rating-left">
+										<i class="fa-solid fa-star"></i>
+									</div>
+									<div className="detail-actions__rating-right">
+										<div className="rating-top">
+											<span className='fw-bold fs-4'>8.9</span>
+											<span>/10</span>
+										</div>
+										<div className="rating-bottom">
+											<span>100k</span>
+										</div>
+									</div>
+								</div>
+								<div className="detail-actions__tooltip">
+
+									<div className="actions__tooltip-item">
+										<div className="actions__tooltip-icon">
+											<i class="fa-solid fa-list"></i>
+										</div>
+									</div>
+									<div className="actions__tooltip-item">
+										<div className="actions__tooltip-icon">
+											<i class="fa-solid fa-heart"></i>
+										</div>
+									</div>
+									<div className="actions__tooltip-item">
+										<div className="actions__tooltip-icon">
+											<i class="fa-solid fa-bookmark"></i>
+										</div>
+									</div>
+									<div className="actions__tooltip-item">
+										<div className="actions__tooltip-icon">
+											<i class="fa-solid fa-star"></i>
+										</div>
+									</div>
+
+								</div>
+								<div className="detail-actions__trailer">
+									<span>Play trailer </span><span><i class="fa-solid fa-play"></i></span>
+								</div>
+							</div>
+							<div className="detail-overview">
+							</div> 
+						</div>
+					</Col>
 				</Row>
 			</div>
 		</div>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import { HashRouter as Router } from 'react-router-dom'
+import App from './App'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <App /> 
       </Router>
     </Provider>
   </React.StrictMode>
